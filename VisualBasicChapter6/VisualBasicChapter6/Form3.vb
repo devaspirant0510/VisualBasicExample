e976d697edd5ec2717200f665b8b5973e5c4887d
Seung <68223593+devaspirant0510@users.noreply.github.com>
2021-04-28 01:20:02 +0900

    구구단 3단마다 줄바꿈해서 보여줌 3중for 문

---
+++ VisualBasicChapter6/VisualBasicChapter6/Form3.vb
@@ -0,0 +1,14 @@
+﻿Public Class Form3
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        TextBox1.Text = ""
+        For i = 1 To 9 Step 3
+            For j = 1 To 9
+                For k = 0 To 2
+                    TextBox1.Text = $"{TextBox1.Text} {i + k} x {j} = {(i + k) * j}      "
+                Next
+                TextBox1.Text = $"{TextBox1.Text}{vbCrLf}"
+            Next
+            TextBox1.Text = $"{TextBox1.Text}{vbCrLf}{vbCrLf}"
+        Next
+    End Sub
+End Class
