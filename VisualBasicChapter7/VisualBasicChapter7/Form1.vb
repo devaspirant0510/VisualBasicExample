fd7a94879411021e78b530f6ecd1d16029b2dd83
Seung <68223593+devaspirant0510@users.noreply.github.com>
2021-04-28 18:48:10 +0900

    구구단 Do while 조건 Loop

---
+++ VisualBasicChapter7/VisualBasicChapter7/Form1.vb
@@ -0,0 +1,13 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim n As Integer = TextBox1.Text
+        TextBox2.Text = ""
+        Dim i As Integer = 1
+        Do While i <= 9
+            TextBox2.Text = $"{TextBox2.Text} {n} x {i} = {n * i} {vbCrLf}"
+            i += 1
+        Loop
+
+
+    End Sub
+End Class
