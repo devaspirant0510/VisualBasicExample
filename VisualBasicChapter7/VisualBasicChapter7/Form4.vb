be7a9b5bc9d54f5b51092e9f7e4a17659050b1f1
Seung <68223593+devaspirant0510@users.noreply.github.com>
2021-04-28 20:03:54 +0900

    Chapter7 Project Setting

---
+++ VisualBasicChapter7/VisualBasicChapter7/Form4.vb
@@ -0,0 +1,11 @@
+﻿Public Class Form4
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim n As Integer = TextBox1.Text
+        Dim i As Integer = 1
+        TextBox2.Text = ""
+        Do
+            TextBox2.Text = $"{TextBox2.Text} {n} x {i} = {n * i} {vbCrLf}"
+            i += 1
+        Loop Until i > 9
+    End Sub
+End Class
