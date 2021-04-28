c0dfc634e51c5b02c93037b16afa305f9c7644f0
Seung <68223593+devaspirant0510@users.noreply.github.com>
2021-04-28 20:45:42 +0900

    1000 interval 마다 이미지 변경

---
+++ VisualBasicChapter9/VisualBasicChapter9/Form2.Designer.vb
@@ -0,0 +1,73 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form2
+    Inherits System.Windows.Forms.Form
+
+    'Form은 Dispose를 재정의하여 구성 요소 목록을 정리합니다.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows Form 디자이너에 필요합니다.
+    Private components As System.ComponentModel.IContainer
+
+    '참고: 다음 프로시저는 Windows Form 디자이너에 필요합니다.
+    '수정하려면 Windows Form 디자이너를 사용하십시오.  
+    '코드 편집기에서는 수정하지 마세요.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.picTraffic = New System.Windows.Forms.PictureBox()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        CType(Me.picTraffic, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(362, 59)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(52, 15)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "신호등"
+        '
+        'picTraffic
+        '
+        Me.picTraffic.Location = New System.Drawing.Point(286, 118)
+        Me.picTraffic.Name = "picTraffic"
+        Me.picTraffic.Size = New System.Drawing.Size(211, 266)
+        Me.picTraffic.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.picTraffic.TabIndex = 1
+        Me.picTraffic.TabStop = False
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        Me.Timer1.Interval = 1000
+        '
+        'Form2
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.picTraffic)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form2"
+        Me.Text = "Form2"
+        CType(Me.picTraffic, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents picTraffic As PictureBox
+    Friend WithEvents Timer1 As Timer
+End Class
---
+++ VisualBasicChapter9/VisualBasicChapter9/Form2.vb
@@ -0,0 +1,31 @@
+﻿Public Class Form2
+    Dim wating As Integer = 0
+    Dim red As Integer = 1
+    Dim yellow As Integer = 2
+    Dim green As Integer = 3
+    Private Sub ChangeImage(Color As Integer)
+        Select Case Color
+            Case wating
+                picTraffic.Image = Image.FromFile(CurDir() + "\신호등(준비중).png")
+            Case red
+                picTraffic.Image = Image.FromFile(CurDir() + "\신호등(적색).png")
+            Case yellow
+                picTraffic.Image = Image.FromFile(CurDir() + "\신호등(노란색).png")
+            Case green
+                picTraffic.Image = Image.FromFile(CurDir() + "\신호등(녹색).png")
+        End Select
+    End Sub
+    Dim time As Integer = 1
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        time += 1
+        ChangeImage(time)
+        If time = 3 Then
+            time = 0
+        End If
+
+    End Sub
+
+    Private Sub Form2_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ChangeImage(wating)
+    End Sub
+End Class
